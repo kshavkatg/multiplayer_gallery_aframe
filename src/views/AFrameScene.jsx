68dd3c946877fb48a340e38420e8b43a2ca7351e
Shavkat <kshavkatg@gmail.com>
2021-12-27 13:31:15 +0600

    add hall glb, position elements, integrate glb model and add teleport points with hover effect

--- src/views/AFrameScene.jsx
+++ src/views/AFrameScene.jsx
@@ -8,6 +8,8 @@ import imageFive from '../images/video_five.png'
 import imageSix from '../images/video_six.png'
 import imageSeven from '../images/video_seven.png'
 
+import hall from '../models/hall_empty.glb'
+
 export default function AFrameScene() {
   useEffect(() => {
   }, [])
@@ -45,158 +47,250 @@ export default function AFrameScene() {
           <source type="video/mp4" src="https://dm0qx8t0i9gc9.cloudfront.net/watermarks/video/5PNlDRM/videoblocks-paris-france-october-01-2017-360-vr-video-lively-rond-point-des-champs-elysees-marcel-dassault-with-walking-people-and-subway-entrance-transport-traffic-around-the-street_S9Im2RfBV__89622c434ad6477a90f8358090371a06__P640.mp4" />
         </video>
         
-        <img id="background1" src="https://cdn.eso.org/images/publicationjpg/ESO_Paranal_360_Marcio_Cabral_Chile_09-CC.jpg" crossOrigin="anonymous"></img>
+        <img id="background1" src="https://cdn.eso.org/images/publicationjpg/ESO_Paranal_360_Marcio_Cabral_Chile_09-CC.jpg" crossOrigin="anonymous" alt='texture'></img>
 
-        <img id="img-one" src={imageOne} crossOrigin="anonymous"></img>
-        <img id="img-two" src={imageTwo} crossOrigin="anonymous"></img>
-        <img id="img-three" src={imageThree} crossOrigin="anonymous"></img>
-        <img id="img-four" src={imageFour} crossOrigin="anonymous"></img>
-        <img id="img-five" src={imageFive} crossOrigin="anonymous"></img>
-        <img id="img-six" src={imageSix} crossOrigin="anonymous"></img>
-        <img id="img-seven" src={imageSeven} crossOrigin="anonymous"></img>
+        <img id="img-one" src={imageOne} crossOrigin="anonymous" alt='texture'></img>
+        <img id="img-two" src={imageTwo} crossOrigin="anonymous" alt='texture'></img>
+        <img id="img-three" src={imageThree} crossOrigin="anonymous" alt='texture'></img>
+        <img id="img-four" src={imageFour} crossOrigin="anonymous" alt='texture'></img>
+        <img id="img-five" src={imageFive} crossOrigin="anonymous" alt='texture'></img>
+        <img id="img-six" src={imageSix} crossOrigin="anonymous" alt='texture'></img>
+        <img id="img-seven" src={imageSeven} crossOrigin="anonymous" alt='texture'></img>
+
+        <a-asset-item id="hall" src={hall}></a-asset-item>
       </a-assets>
       
       
       <a-camera 
         id="camera"
         look-controls="enabled: true; mouseEnabled: true; touchEnabled: true; magicWindowTrackingEnabled: true;"
-        wasd-controls-enabled="false">
+        wasd-controls-enabled="true">
 
         <a-entity cursor="fuse: false; fuseTimeout: 500;"
             raycaster="objects: .collidable;"
             position="0 0 -1"
-            geometry="primitive: ring; radiusInner: 0.01; radiusOuter: 0.02"
+            geometry="primitive: ring; radiusInner: 0.005; radiusOuter: 0.01"
             material="color: yellow; shader: flat"></a-entity>
         
       </a-camera>
-      
+
+      <a-gltf-model src="#hall"></a-gltf-model>
+
+  {/* TELEPORT CIRCLES */}
+      <a-entity 
+        class="collidable"
+        id="teleport_point"
+        position="5.5 0.1 2"
+        rotation="90 0 0"
+        animation__mouseenter="property: scale;  to: 1.4 1.4 1.4; startEvents: mouseenter; dur: 200"
+        animation__mouseleave="property: scale;  to: 1 1 1; startEvents: mouseleave; dur: 200">
+        <a-entity
+            class="collidable"
+            geometry="primitive: ring; radiusInner: 0.5; radiusOuter: 0.4"
+            material="color: white; shader: flat; side: double"></a-entity>
+        <a-entity
+            position="0 0 0.01"
+            class="collidable"
+            geometry="primitive: circle; radius: 0.5"
+            material="color: black; shader: flat; side: double;"
+            animation__mouseenter="property: material.color;  to: #00ff00; startEvents: mouseenter; dur: 200"
+            animation__mouseleave="property: material.color;  to: #0f0f0f; startEvents: mouseleave; dur: 200"></a-entity>
+      </a-entity>
+
+      <a-entity 
+        class="collidable"
+        id="teleport_point"
+        position="5.5 0.1 -4"
+        rotation="90 0 0"
+        animation__mouseenter="property: scale;  to: 1.4 1.4 1.4; startEvents: mouseenter; dur: 200"
+        animation__mouseleave="property: scale;  to: 1 1 1; startEvents: mouseleave; dur: 200">
+        <a-entity
+            class="collidable"
+            geometry="primitive: ring; radiusInner: 0.5; radiusOuter: 0.4"
+            material="color: white; shader: flat; side: double"></a-entity>
+        <a-entity
+            position="0 0 0.01"
+            class="collidable"
+            geometry="primitive: circle; radius: 0.5"
+            material="color: black; shader: flat; side: double;"
+            animation__mouseenter="property: material.color;  to: #00ff00; startEvents: mouseenter; dur: 200"
+            animation__mouseleave="property: material.color;  to: #0f0f0f; startEvents: mouseleave; dur: 200"></a-entity>
+      </a-entity>
+
+      <a-entity 
+        class="collidable"
+        id="teleport_point_one"
+        position="-1 0.1 -4"
+        rotation="90 0 0"
+        animation__mouseenter="property: scale;  to: 1.4 1.4 1.4; startEvents: mouseenter; dur: 200"
+        animation__mouseleave="property: scale;  to: 1 1 1; startEvents: mouseleave; dur: 200">
+        <a-entity
+            class="collidable"
+            geometry="primitive: ring; radiusInner: 0.5; radiusOuter: 0.4"
+            material="color: white; shader: flat; side: double"></a-entity>
+        <a-entity
+            position="0 0 0.01"
+            class="collidable"
+            geometry="primitive: circle; radius: 0.5"
+            material="color: black; shader: flat; side: double;"
+            animation__mouseenter="property: material.color;  to: #00ff00; startEvents: mouseenter; dur: 200"
+            animation__mouseleave="property: material.color;  to: #0f0f0f; startEvents: mouseleave; dur: 200"></a-entity>
+      </a-entity>
+
+      <a-entity 
+        class="collidable"
+        id="teleport_point_one"
+        position="0 0.1 0"
+        rotation="90 0 0"
+        animation__mouseenter="property: scale;  to: 1.4 1.4 1.4; startEvents: mouseenter; dur: 200"
+        animation__mouseleave="property: scale;  to: 1 1 1; startEvents: mouseleave; dur: 200">
+        <a-entity
+            class="collidable"
+            geometry="primitive: ring; radiusInner: 0.5; radiusOuter: 0.4"
+            material="color: white; shader: flat; side: double"></a-entity>
+        <a-entity
+            position="0 0 0.01"
+            class="collidable"
+            geometry="primitive: circle; radius: 0.5"
+            material="color: black; shader: flat; side: double;"
+            animation__mouseenter="property: material.color;  to: #00ff00; startEvents: mouseenter; dur: 200"
+            animation__mouseleave="property: material.color;  to: #0f0f0f; startEvents: mouseleave; dur: 200"></a-entity>
+      </a-entity>
+  {/* END TELEPORT CIRCLES */}
+  {/* HOMEWORLD ELEMENTS    */}
       <a-entity id="homeworld">
 
         <a-entity 
           id="sphere_group_one"
-          position="-6.4 2.8 -2.8"
+          position="-4 1.5 -2"
           animation__mouseenter="property: scale;  to: 1.2 1.2 1.2; startEvents: mouseenter; dur: 200"
           animation__mouseleave="property: scale;  to: 1 1 1; startEvents: mouseleave; dur: 200">
           <a-sphere 
                 id="sphere_one"
+                scale="0.4 0.4 0.4"
                 class="collidable menu"
                 material="src: #img-one" 
                 sphereexpand="videoSrc: video_one;"></a-sphere>
           <a-sphere
                 id="sphere_orange_one"
-                scale="1.1 1.1 1.1"
+                scale="0.42 0.42 0.42"
                 material="color: orange; side: back;"></a-sphere>
-          <a-entity billboard="targetCameraId: camera;" text="wrapCount: 12; width: 1.5; align: center; anchor: center; value: MAJESTIC ROCKY COAST" scale="1.5 1.5 1.5" position="0 -1.7 0"></a-entity>
+          <a-entity billboard="targetCameraId: camera;" text="wrapCount: 12; width: 1.5; align: center; anchor: center; value: MAJESTIC ROCKY COAST" scale="0.5 0.5 0.5" position="0 -0.6 0"></a-entity>
         </a-entity>
 
         <a-entity 
           id="sphere_group_two"
-          position="-4.4 2.1 -5.2"
+          position="-2 1.5 -5"
           animation__mouseenter="property: scale;  to: 1.2 1.2 1.2; startEvents: mouseenter; dur: 200"
           animation__mouseleave="property: scale;  to: 1 1 1; startEvents: mouseleave; dur: 200">
           <a-sphere
                 id="sphere_two" 
+                scale="0.4 0.4 0.4"
                 material="src: #img-two" 
                 class="collidable menu"
                 sphereexpand="videoSrc: video_two;"></a-sphere>
           <a-sphere
                 id="sphere_orange_one"
-                scale="1.1 1.1 1.1"
+                scale="0.42 0.42 0.42"
                 material="color: orange; side: back;"></a-sphere>
-          <a-entity billboard="targetCameraId: camera;"  text="wrapCount: 12; width: 1.5; align: center; anchor: center; value: PARIS FRANCE;" scale="1.5 1.5 1.5" position="0 -1.7 0"></a-entity>
+          <a-entity billboard="targetCameraId: camera;"  text="wrapCount: 12; width: 1.5; align: center; anchor: center; value: PARIS FRANCE;" scale="0.5 0.5 0.5" position="0 -0.6 0"></a-entity>
         </a-entity>
 
         <a-entity 
           id="sphere_group_three"
-          position="-1.2 2 -7.1 "
+          position="1 1.5 -5"
           animation__mouseenter="property: scale;  to: 1.2 1.2 1.2; startEvents: mouseenter; dur: 200"
           animation__mouseleave="property: scale;  to: 1 1 1; startEvents: mouseleave; dur: 200">
           <a-sphere 
                 id="sphere_three"
-                scale="0.8 0.8 0.8"
+                scale="0.4 0.4 0.4"
                 class="collidable menu" 
                 material="src: #img-three" 
                 sphereexpand="videoSrc: video_three;"></a-sphere>
           <a-sphere
                 id="sphere_orange_one"
-                scale="0.9 0.9 0.9"
+                scale="0.42 0.42 0.42"
                 material="color: orange; side: back;"></a-sphere>
-          <a-entity billboard="targetCameraId: camera;"  text="wrapCount: 12; width: 1.5; align: center; anchor: center; value: WHITE YACHT SAILING;" scale="1.5 1.5 1.5" position="0 -1.4 0"></a-entity>
+          <a-entity billboard="targetCameraId: camera;"  text="wrapCount: 12; width: 1.5; align: center; anchor: center; value: WHITE YACHT SAILING;" scale="0.5 0.5 0.5" position="0 -0.6 0"></a-entity>
         </a-entity>
 
         <a-entity 
           id="sphere_group_four"
-          position="2.1 2.8 -6.8 "
+          position="4 1.5 -5"
           animation__mouseenter="property: scale;  to: 1.2 1.2 1.2; startEvents: mouseenter; dur: 200"
           animation__mouseleave="property: scale;  to: 1 1 1; startEvents: mouseleave; dur: 200">
           <a-sphere 
                 id="sphere_four"
+                scale="0.4 0.4 0.4"
                 class="collidable menu"
                 material="src: #img-four" 
                 sphereexpand="videoSrc: video_four;"></a-sphere>
           <a-sphere
                 id="sphere_orange_one"
-                scale="1.1 1.1 1.1"
+                scale="0.42 0.42 0.42"
                 material="color: orange; side: back;"></a-sphere>
-          <a-entity billboard="targetCameraId: camera;"  text="wrapCount: 12; width: 1.5; align: center; anchor: center; value: NAPLES ITALY;" scale="1.5 1.5 1.5" position="0 -1.7 0"></a-entity>
+          <a-entity billboard="targetCameraId: camera;"  text="wrapCount: 12; width: 1.5; align: center; anchor: center; value: NAPLES ITALY;" scale="0.5 0.5 0.5" position="0 -0.6 0"></a-entity>
         </a-entity>
 
         <a-entity 
           id="sphere_group_five"
-          position="4.1 1.7 -6.5 "
+          position="7 1.5 -4"
           animation__mouseenter="property: scale;  to: 1.2 1.2 1.2; startEvents: mouseenter; dur: 200"
           animation__mouseleave="property: scale;  to: 1 1 1; startEvents: mouseleave; dur: 200">
           <a-sphere 
                 id="sphere_five"
                 class="collidable menu"
-                scale="0.7 0.7 0.7"
+                scale="0.4 0.4 0.4"
                 material="src: #img-five" 
                 sphereexpand="videoSrc: video_five;"></a-sphere>
           <a-sphere
                 id="sphere_orange_one"
-                scale="0.8 0.8 0.8"
+                scale="0.42 0.42 0.42"
                 material="color: orange; side: back;"></a-sphere>
-          <a-entity billboard="targetCameraId: camera;"  text="wrapCount: 12; width: 1.5; align: center; anchor: center; value: VENICE ITALY;" scale="1.5 1.5 1.5" position="0 -1.2 0"></a-entity>
+          <a-entity billboard="targetCameraId: camera;"  text="wrapCount: 12; width: 1.5; align: center; anchor: center; value: VENICE ITALY;" scale="0.5 0.5 0.5" position="0 -0.6 0"></a-entity>
         </a-entity>
 
         <a-entity 
           id="sphere_group_six"
-          position="5.3 4.3 -4.8 "
+          position="7 1.5 -1"
           animation__mouseenter="property: scale;  to: 1.2 1.2 1.2; startEvents: mouseenter; dur: 200"
           animation__mouseleave="property: scale;  to: 1 1 1; startEvents: mouseleave; dur: 200">
           <a-sphere 
                 id="sphere_five"
+                scale="0.4 0.4 0.4"
                 class="collidable menu"
                 material="src: #img-six" 
                 sphereexpand="videoSrc: video_six;"></a-sphere>
           <a-sphere
                 id="sphere_orange_one"
-                scale="1.1 1.1 1.1"
+                scale="0.42 0.42 0.42"
                 material="color: orange; side: back;"></a-sphere>
-          <a-entity billboard="targetCameraId: camera;"  text="wrapCount: 12; width: 1.5; align: center; anchor: center; value: FRANKFURT GERMANY;" scale="1.5 1.5 1.5" position="0 -1.7 0"></a-entity>
+          <a-entity billboard="targetCameraId: camera;"  text="wrapCount: 12; width: 1.5; align: center; anchor: center; value: FRANKFURT GERMANY;" scale="0.5 0.5 0.5" position="0 -0.6 0"></a-entity>
         </a-entity>
 
         <a-entity 
           id="sphere_group_seven"
-          position="6.8 2.9 -2.4 "
+          position="7 1.5 2 "
           animation__mouseenter="property: scale;  to: 1.2 1.2 1.2; startEvents: mouseenter; dur: 200"
           animation__mouseleave="property: scale;  to: 1 1 1; startEvents: mouseleave; dur: 200">
           <a-sphere 
-                id="sphere_four" 
+                id="sphere_four"
+                scale="0.4 0.4 0.4"
                 class="collidable menu"
                 material="src: #img-seven" 
                 sphereexpand="videoSrc: video_seven;"></a-sphere>
           <a-sphere
                 id="sphere_orange_one"
-                scale="1.1 1.1 1.1"
+                scale="0.42 0.42 0.42"
                 material="color: orange; side: back;"></a-sphere>
-          <a-entity billboard="targetCameraId: camera;"  text="wrapCount: 12; width: 1.5; align: center; anchor: center; value: VALENCIA SPAIN;" scale="1.5 1.5 1.5" position="0 -1.7 0"></a-entity>
+          <a-entity billboard="targetCameraId: camera;"  text="wrapCount: 12; width: 1.5; align: center; anchor: center; value: VALENCIA SPAIN;" scale="0.5 0.5 0.5" position="0 -0.6 0"></a-entity>
         </a-entity>
 
       </a-entity>
-      
+    {/* END HOMEWORLD ELEMENTS */}
+    
       <a-entity position="0 5 -6 " id="back" visible="false">
-          <a-entity billboard="targetCameraId: camera;" text="width: 5; align: center; anchor: center; value: GO BACK;" scale="1.5 1.5 1.5" position="0 -1.7 0"></a-entity>
+          <a-entity billboard="targetCameraId: camera;" text="width: 5; align: center; anchor: center; value: GO BACK;" scale="0.5 0.5 0.5" position="0 -0.6 0"></a-entity>
           <a-sphere
                 id="back_sphere"
                 animation__mouseenter="property: scale;  to: 1.2 1.2 1.2; startEvents: mouseenter; dur: 200"
@@ -204,8 +298,6 @@ export default function AFrameScene() {
                 class="collidable"></a-sphere>
       </a-entity>
       
-      <a-sky id="homeworld" src="#background1" rotation="0 0 0"></a-sky>
-
       <a-videosphere id="videosphere" visible="false" rotation="0 0 0" src="#video_one"></a-videosphere>
 
       <a-sphere
